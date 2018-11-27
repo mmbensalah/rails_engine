c536a9237172fa9aed275f652720a5b510394773
mmbensalah <mmbensalah@gmail.com>
2018-11-27 16:57:35 -0700

    creates customer table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_151156) do
+ActiveRecord::Schema.define(version: 2018_11_27_235543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "merchants", force: :cascade do |t|
     t.string "name"
