eee30addbc3c5eba019917a95e6f802a43d1405e
mmbensalah <mmbensalah@gmail.com>
2018-11-28 16:49:17 -0700

    adds AR method to merchants, creates merchant serializer, creates most revenue controller

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,8 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
 
-  def most_revenue(quantity = 5)
-    .select("merchants.*, SUM(invoice_items.quantity * items.unit_price) AS total_revenue")
+  def self.most_revenue(quantity = 5)
+    select("merchants.*, SUM(invoice_items.quantity * items.unit_price) AS total_revenue")
     .joins(items: :invoice_items)
     .group(:id)
     .order("total_revenue DESC")
