5f5a2c85c94248b962998962bea492a602c4ce13
mmbensalah <mmbensalah@gmail.com>
2018-11-28 14:32:58 -0700

    adds top merchants by x method to merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,12 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def most_revenue(quantity = 5)
+    .select("merchants.*, SUM(invoice_items.quantity * items.unit_price) AS total_revenue")
+    .joins(items: :invoice_items)
+    .group(:id)
+    .order("total_revenue DESC")
+    .limit(quantity)
+  end
 end
