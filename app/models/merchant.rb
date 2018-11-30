fec45440ae483f74e6c99948b1ba965a748792d4
mmbensalah <mmbensalah@gmail.com>
2018-11-30 14:30:10 -0700

    adds controller, endpoint and serializer for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,10 +3,30 @@ class Merchant < ApplicationRecord
   has_many :invoices
 
   def self.most_revenue(quantity = 5)
+    unscoped.
     select("merchants.*, SUM(invoice_items.quantity * items.unit_price) AS total_revenue")
     .joins(items: :invoice_items)
     .group(:id)
     .order("total_revenue DESC")
     .limit(quantity)
+  end
+
+  def self.most_items(quantity = 5)
+    unscoped.
+    select("merchants.*, SUM(invoice_items.quantity) AS total_items")
+    .joins(invoices: [:invoice_items, :transactions])
+    .merge(Transaction.unscoped.successful)
+    .group(:id)
+    .order("total_items DESC")
+    .limit(quantity)
+  end
+
+  def self.revenue_by_date(date)
+    unscoped.
+    select("merchants.*, SUM(invoice_items.quantity * items.unit_price) AS total_revenue")
+    .joins(items: :invoice_items)
+    .where(invoices: {created_at: date.to_date.beginning_of_day..date.to_date.end_of_day})
+    .group(:id)
+    .order(:id)
   end
 end
