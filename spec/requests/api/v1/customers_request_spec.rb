f55c5f82e93f47d44b59c4c4df5d057dfe82b526
mmbensalah <mmbensalah@gmail.com>
2018-12-02 12:42:45 -0700

    creates find endpoints for customer name, id. name search not case insensitive

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -27,4 +27,40 @@ describe 'Customers API' do
     expect(customer["data"]["attributes"]["first_name"]).to eq(first_name)
     expect(customer["data"]["attributes"]["last_name"]).to  eq(last_name)
   end
+
+  it 'returns a customer by searching by first name' do
+    customer = Customer.create(id: 1, first_name: "Rainy", last_name: "Day")
+
+    get "/api/v1/customers/find?first_name=#{customer.first_name}"
+
+    expect(response).to be_successful
+
+    customer_parsed = JSON.parse(response.body)
+
+    expect(customer_parsed["data"][0]["attributes"]["first_name"]).to eq(customer.first_name)
+  end
+
+  it 'returns a customer by searching by last name' do
+    customer = Customer.create(id: 1, first_name: "Rainy", last_name: "Day")
+
+    get "/api/v1/customers/find?last_name=#{customer.last_name}"
+
+    expect(response).to be_successful
+
+    customer_parsed = JSON.parse(response.body)
+
+    expect(customer_parsed["data"][0]["attributes"]["last_name"]).to eq(customer.last_name)
+  end
+
+  it 'returns a customer by searching for id' do
+    customer = Customer.create(id: 1, first_name: "Rainy", last_name: "Day")
+
+    get "/api/v1/customers/find?id=#{customer.id}"
+
+    expect(response).to be_successful
+
+    customer_parsed = JSON.parse(response.body)
+
+    expect(customer_parsed["data"][0]["attributes"]["id"]).to eq(customer.id)
+  end
 end
