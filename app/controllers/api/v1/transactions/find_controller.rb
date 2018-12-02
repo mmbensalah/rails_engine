e35f63119306bf990e2311a557dd44ec18e00616
mmbensalah <mmbensalah@gmail.com>
2018-12-02 15:28:22 -0700

    Edits find endpoint & test for transactions so that serialized output is a hash, creates endpoint/test for find_all

--- app/controllers/api/v1/transactions/find_controller.rb
+++ app/controllers/api/v1/transactions/find_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Transactions::FindController < ApplicationController
+  def show
+    render json: TransactionSerializer.new(Transaction.find_by(transaction_params))
+  end
+
   def index
     render json: TransactionSerializer.new(Transaction.where(transaction_params))
   end
