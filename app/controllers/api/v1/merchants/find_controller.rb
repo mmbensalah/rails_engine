83e6d7b6909d645660c96491ee833f238986a459
mmbensalah <mmbensalah@gmail.com>
2018-12-02 14:55:32 -0700

    Edits find endpoint for merchant so that serialized output is a hash, creates endpoint/test for find_all

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::Merchants::FindController < ApplicationController
+  def show
+    render json: MerchantSerializer.new(Merchant.find_by(merchant_params))
+  end
+
   def index
     render json: MerchantSerializer.new(Merchant.where(merchant_params))
   end
